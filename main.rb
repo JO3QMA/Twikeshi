9d2eb58315cbff454fbf426615b682a815e8e078
JO3QMA <g.dark.matter@gmail.com>
2020-05-13 00:28:08 +0900

    リファクタリングした。 ハッシュタグ周りがおかしい。

--- main.rb
+++ main.rb
@@ -8,118 +8,132 @@ require 'pp'
 require 'yaml'
 require 'date'
 
-# コマンドオプション
-# -h ハッシュタグ  : ハッシュタグを含むツイートを除く
-# -r リツイート    : 指定した数値以上のRTされたツイートを除く
-# -f ふぁぼ       :　指定した数値以上のFavされたツイートを除く
-# -t 時間         : 指定した時間以後のツイートを除く
-
-config = YAML.load_file('./config.yml')
-api = config['api']
-option = config['option']
-def unzip(file = 'twitter.zip')
-  
-dest = 'tmp/'
-  Zip::File.open(file) do |zip|
-    zip.each do |entry|
-      p entry.name
-      zip.extract(entry, dest + entry.name) { true }
+class TweetDeleter
+  def initialize
+    puts '初期処理をしています。'
+    @tmp_dir = 'tmp/'
+    yaml_file = YAML.load_file('./config.yml')
+    api = yaml_file['api']
+    @option = yaml_file['option']
+    @until_time = Date.parse(@option['Until']) unless @option['Until'].empty?
+    @since_time = Date.parse(@option['Since']) unless @option['Since'].empty?
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = api['API_Key']
+      config.consumer_secret     = api['API_Secret_Key']
+      config.access_token        = api['Access_Token']
+      config.access_token_secret = api['Access_Token_Secret']
     end
+    @allow_array = []
+    @deny_array = []
+    puts '初期処理を完了しました。'
   end
-end
 
-def delete_tmp(dir = 'tmp/')
-  FileUtils.rm_r(dir)
-end
-
-def reformat_json
-  file = File.open('tmp/data/tweet.js', 'r')
-  buffer = file.read
-  buffer.gsub!('window.YTD.tweet.part0 = [ {', '[ {')
-  file = File.open('tmp/data/tweet.js', 'w')
-  file.write(buffer)
-  file.close
-end
-
-def load_json
-  tweet_json = 'tmp/data/tweet.js'
-  json_data = open(tweet_json) do |io|
-    JSON.load(io)
-  end
-  json_data
-end
-unzip
-reformat_json
-test = load_json
-p load_json.count
-id_array = []
-deny_array = []
-test.each do |i|
-  retweet = i['tweet']['retweet_count'].to_i
-  favorite = i['tweet']['favorite_count'].to_i
-  hashtags = i['tweet']['entities']['hashtags']
-  created_at = Date.parse(i['tweet']['created_at'])
-  if !option['Until'].empty?
-    until_time = Date.parse(option['Until'])
-  end
-  if !option['Since'].empty?
-    since_time = Date.parse(option['Since'])
-  end
-  if !option['RT'] == -1 && retweet > option['RT']
-    deny_array.push i['tweet']['id']
-  elsif !option['Fav'] == -1 && favorite > option['Fav']
-    deny_array.push i['tweet']['id']
-  elsif !option['Hashtag'].empty? && hashtags.empty?
-    found = false
-    hashtags.each do |hashtag_hash|
-      if hashtag_hash['text'] == option['Hashtag']
-        found = true
-        break
-      else
-        found = false
+  def unzip(file = 'twitter.zip')
+    puts "#{file}を展開しています。"
+    Zip::File.open(file) do |zip|
+      zip.each do |entry|
+        zip.extract(entry, @tmp_dir + entry.name) { true }
       end
     end
-    if found == true
-      deny_array.push i['tweet']['id']
-    else
-      id_array.push i['tweet']['id']
+    puts '展開を完了しました。'
+  end
+
+  def make_json
+    puts 'tweet.jsからJSONを生成しています。'
+    file = File.open(@tmp_dir + 'data/tweet.js', 'r')
+    buffer = file.read
+    buffer.gsub!('window.YTD.tweet.part0 = [ {', '[ {')
+    file = File.open(@tmp_dir + 'data/tweet.js', 'w')
+    file.write(buffer)
+    file.close
+    puts 'JSONの生成が完了しました。'
+  end
+
+  def load_json
+    puts 'JSONのロードを開始しています。'
+    file = open(@tmp_dir + 'data/tweet.js') do |io|
+      JSON.load(io)
     end
-  elsif !option['Until'].empty? && created_at < until_time
-    deny_array.push i['tweet']['id']
-  elsif !option['Since'].empty? && created_at > since_time
-    deny_array.push i['tweet']['id']
-  else
-    id_array.push i['tweet']['id']
+    puts 'JSONのロードを完了しました。'
+    puts "#{file.count}件のツイートを読み込み終わりました。"
+    file
+  end
+
+  def seach_tweet(tweets)
+    puts 'ツイートの除外検索を開始しています。'
+    tweets.each do |tweet|
+      retweet = tweet['tweet']['retweet_count'].to_i
+      favorite = tweet['tweet']['favorite_count'].to_i
+      hashtags = tweet['tweet']['entities']['hashtags']
+      created_at = Date.parse(tweet['tweet']['created_at'])
+      if !@option['RT'] == -1 && retweet > @option['RT']
+        deny_pusher(tweet)
+      elsif !@option['Fav'] == -1 && favorite > @option['Fav']
+        deny_pusher(tweet)
+      elsif !@option['Hashtag'].empty? && !hashtags.empty?
+        hit = false
+        hashtags.each do |hashtag|
+          if hashtag['text'] == @option['Hashtag']
+            hit = true
+          end
+        end
+        if hit == true
+          deny_pusher(tweet)
+        else
+          allow_pusher(tweet) # ここがおかしい。ハッシュタグ含んでいて条件に引っかからなかったら問答無用で消されるのはおかしい
+        end
+      elsif !@option['Until'].empty? && created_at < @until_time
+        deny_pusher(tweet)
+      elsif !@option['Since'].empty? && created_at > @since_time
+        deny_pusher(tweet)
+      else
+        allow_pusher(tweet)
+      end
+    end
+    puts 'ツイートの検索を終了しました。'
+    puts "検索件数: #{tweets.count}件"
+    puts "除外件数: #{@deny_array.count}件"
+    puts "削除件数: #{@allow_array.count}件"
+  end
+
+  def allow_pusher(tweet)
+    @allow_array.push tweet['tweet']['id']
+  end
+
+  def deny_pusher(tweet)
+    @deny_array.push tweet['tweet']['id']
+  end
+
+  def delete_tweets
+    count = 0
+    @allow_array.each do |tweet|
+      count += 1
+      #@client.destroy(tweet)
+      progress(count)
+    end
+  end
+
+  def progress(count)
+    percent = count / @allow_array.count.to_f * 100
+    scale = 2
+    bar = percent / scale
+    hide_bar = 100 /scale - bar.floor
+    print "\r#{count}件目 [#{'=' * bar}#{' ' * hide_bar}] #{percent.floor(1)}%完了"
+    puts '' if count == @allow_array.count
+  end
+
+  def delete_tmp_dir
+    FileUtils.rm_r(@tmp_dir)
+  end
+
+  def main
+    unzip
+    make_json
+    seach_tweet(load_json)
+    delete_tweets
+    delete_tmp_dir
+    pp @allow_array
   end
 end
-
-p id_array.count
-pp deny_array
-p deny_array.count
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = api['API_Key']
-  config.consumer_secret     = api['API_Secret_Key']
-  config.access_token        = api['Access_Token']
-  config.access_token_secret = api['Access_Token_Secret']
-end
-
-def destroy(array)
-  count = 0
-  array.each do |tweet_id|
-    count += 1
-    client.destroy(tweet_id)
-    progress(count)
-  end
-end
-
-def progress(count)
-  count -= 1
-  percent = count / load_json.count.to_f * 100
-  scale = 2
-  bar = percent / scale
-  hide_bar = 100 / scale - bar.floor
-  print "\r#{count}件目 [#{'=' * bar}#{' ' * hide_bar}] #{percent.floor(1)}%完了"
-  puts '' if count == load_json.count
-end
- destroy(id_array)
- delete_tmp
+test = TweetDeleter.new
+test.main
